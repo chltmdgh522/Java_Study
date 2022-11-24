46117c2c25d90af01b9fbea97c9124223a7030ad
chltmdgh522 <chltmdgh517@naver.com>
2022-11-25 00:38:01 +0900

    test3

---
+++ Seungho/src/test3/StudentTest.java
@@ -0,0 +1,17 @@
+package test3;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		
+		Student choi=new Student("최승호", 01);
+		
+		choi.addSubject("국어",95);
+		
+		choi.addSubject("수학",95);
+		
+		choi.show();
+		
+		
+	}
+}
