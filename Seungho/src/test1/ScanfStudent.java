3752d5bdcabd7c4f2f371cd9e8befe25ce480637
chltmdgh522 <chltmdgh517@naver.com>
2022-11-25 00:42:54 +0900

    코드 포맷 정렬함

--- Seungho/src/test1/ScanfStudent.java
+++ Seungho/src/test1/ScanfStudent.java
@@ -3,17 +3,17 @@ package test1;
 public class ScanfStudent {
 
     public static void main(String[] args) {
-        StudentInformation choi = new StudentInformation("최승호", 01);
+        StudentInformation choi = new StudentInformation("최승호", 1);
         choi.setKorea("국어", 100);
         choi.setMath("수학", 100);
         choi.setEnglish("영어", 100);
 
-        StudentInformation kim = new StudentInformation("김승호", 02);
+        StudentInformation kim = new StudentInformation("김승호", 2);
         kim.setKorea("국어", 100);
         kim.setMath("수학", 100);
         kim.setEnglish("영어", 100);
 
-        StudentInformation park = new StudentInformation("박승호", 03);
+        StudentInformation park = new StudentInformation("박승호", 3);
         park.setKorea("국어", 100);
         park.setMath("수학", 100);
         park.setEnglish("영어", 100);
