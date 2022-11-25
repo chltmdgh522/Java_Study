54c275a07770592eca78c1d670e6bbae2976dd36
chltmdgh522 <chltmdgh517@naver.com>
2022-11-26 00:09:46 +0900

    코드 포맷 정렬함

--- Chapter5/src/reference/StudentTest.java
+++ Chapter5/src/reference/StudentTest.java
@@ -2,17 +2,17 @@ package reference;
 
 public class StudentTest {
 
-	public static void main(String[] args) {
-		Student studentChoi=new Student(100,"최승호");
-		studentChoi.setKorea("국어", 100);
-		studentChoi.setMath("수학", 100);
-		
-		Student studentKim=new Student(101,"김승호");
-		studentKim.setKorea("국어", 1000);
-		studentKim.setMath("수학", 10);
-		
-		studentChoi.showStudentInfor();
-		studentKim.showStudentInfor();
-	}
+    public static void main(String[] args) {
+        Student studentChoi = new Student(100, "최승호");
+        studentChoi.setKorea("국어", 100);
+        studentChoi.setMath("수학", 100);
+
+        Student studentKim = new Student(101, "김승호");
+        studentKim.setKorea("국어", 1000);
+        studentKim.setMath("수학", 10);
+
+        studentChoi.showStudentInfor();
+        studentKim.showStudentInfor();
+    }
 
 }
