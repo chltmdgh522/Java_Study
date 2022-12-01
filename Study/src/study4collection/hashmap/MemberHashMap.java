808e46a6cf777ecaea0b78c4f22fe4e0287ebc98
chltmdgh522 <chltmdgh517@naver.com>
2022-12-02 00:05:48 +0900

    HashMap

---
+++ Study/src/study4collection/hashmap/MemberHashMap.java
@@ -0,0 +1,36 @@
+package study4collection.hashmap;
+
+import study4collection.Member;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class MemberHashMap {
+
+    HashMap<Integer, Member> hashMap = new HashMap<Integer, Member>();
+
+    public void addHashMap(Member member) {
+        hashMap.put(member.getId(), member);
+    }
+
+    public boolean removeHashMap(int id) {
+        if (hashMap.containsKey(id)) {
+            hashMap.remove(id);
+            return true;
+        }
+        return false;
+    }
+
+    public void show() {
+        System.out.println(hashMap);
+        Iterator<Integer> ir = hashMap.keySet().iterator();
+
+        while (ir.hasNext()) {
+            int key = ir.next();
+            Member member = hashMap.get(key);
+            System.out.println(member);
+            System.out.println(key);
+        }
+    }
+
+}
