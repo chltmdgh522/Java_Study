6dabca14b848f2296ae72ecce43958301b3caf59
chltmdgh522 <chltmdgh517@naver.com>
2022-12-03 14:04:34 +0900

    Test7

---
+++ Seungho/src/test7/Main.java
@@ -0,0 +1,32 @@
+package test7;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+	
+		
+		Player p1=new Player();
+		p1.jumpEx(1);
+		
+		System.out.println("===============");
+		
+		Silver s=new Silver("이민수");
+		p1.upgradeLevel(s);
+		p1.jumpEx(2);
+		
+		System.out.println("===============");
+		
+		Gold g=new Gold("김용현");
+		p1.upgradeLevel(g);
+		p1.jumpEx(3);
+
+		
+		
+		
+		
+	}
+
+}
