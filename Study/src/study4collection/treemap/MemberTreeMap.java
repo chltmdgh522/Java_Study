419535b5e65662583d82e106a72b4f5dce6d76b8
chltmdgh522 <chltmdgh517@naver.com>
2022-12-06 00:11:01 +0900

    TreeMap

---
+++ Study/src/study4collection/treemap/MemberTreeMap.java
@@ -0,0 +1,40 @@
+package study4collection.treemap;
+
+import study4collection.Member;
+
+import java.util.Iterator;
+import java.util.TreeMap;
+
+public class MemberTreeMap {
+
+    TreeMap<Integer, Member> treeMap=new TreeMap<Integer, Member>(new Member());
+
+    public void addTreeMap(Member member){
+        treeMap.put(member.getId(),member);
+    }
+
+    public boolean removeTreeMap(int id){
+        Iterator<Integer> ir= treeMap.keySet().iterator();
+        while(ir.hasNext()){
+            int key=ir.next();
+            if(id==key){
+                ir.remove();
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public void show(){
+        Iterator<Integer> ir=treeMap.keySet().iterator();
+
+        while(ir.hasNext()){
+            int key= ir.next();
+            Member a= treeMap.get(key); // key 값을 넣어줌으로써 Member의 값을 불러올수있다.
+            System.out.println(a);
+
+        }
+    }
+
+}
---
+++ Study/src/study4collection/treemap/TreeMapTest.java
@@ -0,0 +1,22 @@
+package study4collection.treemap;
+
+import study4collection.Member;
+
+public class TreeMapTest {
+    public static void main(String[] args) {
+
+        MemberTreeMap memberTreeMap=new MemberTreeMap();
+        Member m1= new Member(1,"최승호");
+        Member m2= new Member(2,"정승호");
+        Member m3= new Member(3,"김승호");
+
+        memberTreeMap.addTreeMap(m1);
+        memberTreeMap.addTreeMap(m2);
+        memberTreeMap.addTreeMap(m3);
+        memberTreeMap.show();
+
+        memberTreeMap.removeTreeMap(1);
+        memberTreeMap.show();
+
+    }
+}
