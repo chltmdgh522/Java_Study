f27fac092ac51e363716a27c1d7dd5d4105e64b6
chltmdgh522 <chltmdgh517@naver.com>
2022-12-09 00:03:49 +0900

    Re stream Study(2)

--- Study/src/study1stream/ReduceTest.java
+++ Study/src/study1stream/ReduceTest.java
@@ -6,27 +6,31 @@ import java.util.function.BinaryOperator;
 class CompareString implements BinaryOperator<String> {
     @Override
     public String apply(String s1, String s2) {
-        if (s1.getBytes().length <= s2.getBytes().length)
+        if (s1.getBytes().length <= s2.getBytes().length) {
             return s1;
-        else return s2;
+        } else return s2;
     }
 }
 
 public class ReduceTest {
     public static void main(String[] args) {
-
-        String[] names = {"조선임금은 그들을 노하였노라", "고려황제는 들어라", "임진왜란과 귀주대첩"};
-
+        String[] names = {"위화도 회군", "살수대첩", "인천 상륙 작전"};
+        //1
         Arrays.stream(names).sorted().forEach(s -> System.out.println(s));
 
-        System.out.println(Arrays.stream(names).reduce((""), (s1, s2) -> {
-            if (s1.getBytes().length <= s2.getBytes().length)
+        //2
+        System.out.println(Arrays.stream(names).reduce("", ((s1, s2) -> {
+            if (s1.getBytes().length >= s2.getBytes().length) {
                 return s1;
-            else return s2;
-        }));
+            } else return s2;
+        }
+        )));
 
+        //3
         System.out.println(Arrays.stream(names).reduce(new CompareString()).get());
     }
 
-
 }
+
+
+
