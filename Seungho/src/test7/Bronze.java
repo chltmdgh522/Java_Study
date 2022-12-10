9a29ee85f927bcd48cc58cfcfb8fd3b261ca1db7
chltmdgh522 <chltmdgh517@naver.com>
2022-12-11 00:32:07 +0900

    Test7(Space Modify)

--- Seungho/src/test7/Bronze.java
+++ Seungho/src/test7/Bronze.java
@@ -2,30 +2,31 @@ package test7;
 
 public class Bronze extends PlayerLevel {
 
-	public Bronze(String name){
-		super(name);
-		
-	}
+    public Bronze(String name) {
+        super(name);
 
-	@Override
-	public void run() {
-		System.out.println("이동속도가 매우 느립니다.");
-		}
+    }
 
-	@Override
-	public void strength() {
-		System.out.println("체력이 다 떨어졌습니다.");
-		
-	}
-	public void showMessage() {
-		System.out.println("레벨이 동입니다.");
-		
-	}
+    @Override
+    public void run() {
+        System.out.println("이동속도가 매우 느립니다.");
+    }
 
-	@Override
-	public void situation() {
-		System.out.println("횟수 높이 수준이 낮아 넘어졌습니다.");
-		
-	}
+    @Override
+    public void strength() {
+        System.out.println("체력이 다 떨어졌습니다.");
+
+    }
+
+    public void showMessage() {
+        System.out.println("레벨이 동입니다.");
+
+    }
+
+    @Override
+    public void situation() {
+        System.out.println("횟수 높이 수준이 낮아 넘어졌습니다.");
+
+    }
 
 }
--- Seungho/src/test7/PlayerLevel.java
+++ Seungho/src/test7/PlayerLevel.java
@@ -1,39 +1,49 @@
 package test7;
 
 public abstract class PlayerLevel {
-	
-	String name;
-	public PlayerLevel(String name){
-		this.name=name;
-	}
-	public abstract void run();
-	public abstract void situation();
-	public abstract void strength();
-	public void showMessage() {}
-	public void gameStart() {
-		System.out.println("게임을 시작합니다.");	
-	}
-	public void gameStop() {
-		System.out.println("게임을 종료합니다.");	
-	}
-	public void name() {
-		System.out.println("용사의 이름은 "+name+"입니다.");	
-	}
-	
-	public void jump(){
-		System.out.println("점프를 합니다.");	
-	}
-	public final void show(int count) {
-		name();
-		gameStart();	
-		run();
-		for(int i=0; i<count; i++) {
-			jump();
-		}
-		situation();
-		strength();
-		gameStop();
-		
-	}
+
+    String name;
+
+    public PlayerLevel(String name) {
+        this.name = name;
+    }
+
+    public abstract void run();
+
+    public abstract void situation();
+
+    public abstract void strength();
+
+    public void showMessage() {
+    }
+
+    public void gameStart() {
+        System.out.println("게임을 시작합니다.");
+    }
+
+    public void gameStop() {
+        System.out.println("게임을 종료합니다.");
+    }
+
+    public void name() {
+        System.out.println("용사의 이름은 " + name + "입니다.");
+    }
+
+    public void jump() {
+        System.out.println("점프를 합니다.");
+    }
+
+    public final void show(int count) {
+        name();
+        gameStart();
+        run();
+        for (int i = 0; i < count; i++) {
+            jump();
+        }
+        situation();
+        strength();
+        gameStop();
+
+    }
 
 }
