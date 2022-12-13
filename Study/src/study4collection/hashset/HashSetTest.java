c964f5afd495cb784a2dcc950c2959df5c53ebdd
chltmdgh522 <chltmdgh517@naver.com>
2022-12-14 00:14:57 +0900

    Study4(hashset-remake)

--- Study/src/study4collection/hashset/HashSetTest.java
+++ Study/src/study4collection/hashset/HashSetTest.java
@@ -4,19 +4,16 @@ import study4collection.Member;
 
 public class HashSetTest {
     public static void main(String[] args) {
+        MemberHashSet m=new MemberHashSet();
+        Member m1 =new Member(2,"최승호");
+        Member m2 =new Member(3,"홍세현");
+        Member m3 =new Member(1,"명승진");
+        m.addHashSet(m1);
+        m.addHashSet(m2);
+        m.addHashSet(m3);
+        m.removeHashSet(1);
 
-        MemberHashSet memberHashSet=new MemberHashSet();
-        Member m1= new Member(1,"승호");
-        Member m2= new Member(2,"용현");
-        Member m3= new Member(2,"용현");
-        memberHashSet.addHashSet(m1);
-        memberHashSet.addHashSet(m2);
-        memberHashSet.addHashSet(m3);
-
-
-
-        memberHashSet.show();
-
+        m.show();
 
 
 
--- Study/src/study4collection/hashset/MemberHashSet.java
+++ Study/src/study4collection/hashset/MemberHashSet.java
@@ -4,8 +4,33 @@ import study4collection.Member;
 
 import java.util.HashSet;
 import java.util.Iterator;
+
 public class MemberHashSet {
-    HashSet<Member> hashSet=new HashSet<>();
+
+    HashSet<Member> hashSet = new HashSet<>();
+
+    public void addHashSet(Member member) {
+        hashSet.add(member);
+    }
+
+    public boolean removeHashSet(int id) {
+        Iterator<Member> ir = hashSet.iterator();
+        while (ir.hasNext()) {
+            Member member = ir.next();
+            if (id == member.getId()) {
+                ir.remove();
+            }
+        }
+        return false;
+    }
+
+    public void show() {
+        for (Member m : hashSet) {
+            System.out.println(m);
+        }
+    }
+}
+  /*  HashSet<Member> hashSet=new HashSet<>();
 
     public void addHashSet(Member member){
         hashSet.add(member);
@@ -28,44 +53,20 @@ public class MemberHashSet {
         for(Member m : hashSet){
             System.out.println("d"+m);
         }
-    }
 
-
-
-
-
-
-
-
-
-
-
-    /*HashSet<Member> hashSet = new HashSet<>();
-
-    public void addHashSet(Member member) {
-        hashSet.add(member);
-    }
-
-
-    public boolean removeHashSet(int id) {
-        Iterator<Member> iterator = hashSet.iterator();
-        while (iterator.hasNext()) {
-            Member member = iterator.next();
-            if (id == member.getId()) {
-                iterator.remove();
-                return true;
-            }
-        }
-        System.out.println(id + "없습니당");
-        return false;
-    }
-
-    public void show() {
-
-        for (Member m : hashSet) {
-            System.out.println(m);
-        }
     }*/
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
