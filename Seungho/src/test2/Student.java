2c0741ddef3b2363eab5ad37a71ad60207cababc
chltmdgh522 <chltmdgh517@naver.com>
2022-12-16 23:39:28 +0900

    Test2(Space Modify)

--- Seungho/src/test2/Student.java
+++ Seungho/src/test2/Student.java
@@ -1,35 +1,36 @@
 package test2;
 
 public class Student {
-	String studentName;
-	int grade;
-	int money;
-	
-	
-	public Student(String studentName, int grade, int money) {
-		this.studentName=studentName;
-	    this.money=money;
-	    this.grade=grade;
-}	
-	public void takeBus(Bus bus)//어떤 버스를 탈건지 그 버스에 대한정보 
-	{
-		money-=1000;
-		bus.take(1000);	// Bus가 객체라서 .을하면 쓸수 있는 멤버변수와 메서드를 볼수 있다. 
-}	
-	
-	public void takeSubway(Subway sub){
-		money-=1000;
-		sub.take(1000);	
-}	
+    String studentName;
+    int grade;
+    int money;
 
-	public void takeTaxi(Taxi taxi){
-		money-=1000;
-		taxi.take(1000);	
-}	
-	
-	public void show() {
-		System.out.println(grade+"학년"+studentName + "님의 남은 돈은" + money +
-		"입니다.");
-}
-	
+
+    public Student(String studentName, int grade, int money) {
+        this.studentName = studentName;
+        this.money = money;
+        this.grade = grade;
+    }
+
+    public void takeBus(Bus bus)//어떤 버스를 탈건지 그 버스에 대한정보
+    {
+        money -= 1000;
+        bus.take(1000);    // Bus가 객체라서 .을하면 쓸수 있는 멤버변수와 메서드를 볼수 있다.
+    }
+
+    public void takeSubway(Subway sub) {
+        money -= 1000;
+        sub.take(1000);
+    }
+
+    public void takeTaxi(Taxi taxi) {
+        money -= 1000;
+        taxi.take(1000);
+    }
+
+    public void show() {
+        System.out.println(grade + "학년" + studentName + "님의 남은 돈은" + money +
+                "입니다.");
+    }
+
 }
--- Seungho/src/test2/TakeTrains.java
+++ Seungho/src/test2/TakeTrains.java
@@ -2,30 +2,30 @@ package test2;
 
 public class TakeTrains {
 
-	public static void main(String[] args) {
-		Student seunghoChoi=new Student("최승호",3,5000);
-		
-		
-		Student minsuLee=new Student("이민수",2,6000);
-		
-		
-		Bus bus100=new Bus(100);
-		Subway subway1=new Subway(1);
-		Taxi taxi11=new Taxi(11);
-		
-		seunghoChoi.takeBus(bus100);
-		seunghoChoi.takeSubway(subway1);
-		seunghoChoi.takeTaxi(taxi11);
-		
-		minsuLee.takeBus(bus100);
-		
-		seunghoChoi.show();
-		minsuLee.show();
-		bus100.show();
-		subway1.show();
-		taxi11.show();
+    public static void main(String[] args) {
+        Student seunghoChoi = new Student("최승호", 3, 5000);
 
-		
-	}
+
+        Student minsuLee = new Student("이민수", 2, 6000);
+
+
+        Bus bus100 = new Bus(100);
+        Subway subway1 = new Subway(1);
+        Taxi taxi11 = new Taxi(11);
+
+        seunghoChoi.takeBus(bus100);
+        seunghoChoi.takeSubway(subway1);
+        seunghoChoi.takeTaxi(taxi11);
+
+        minsuLee.takeBus(bus100);
+
+        seunghoChoi.show();
+        minsuLee.show();
+        bus100.show();
+        subway1.show();
+        taxi11.show();
+
+
+    }
 
 }
