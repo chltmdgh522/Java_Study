57277c39727283646b79ab192817fcf645208af9
chltmdgh522 <chltmdgh517@naver.com>
2022-12-18 00:15:53 +0900

    Test3(Space Modify)

--- Seungho/src/test3/Subject.java
+++ Seungho/src/test3/Subject.java
@@ -1,28 +1,26 @@
 package test3;
 
 public class Subject {
+
+    String subjectName;
+    int subjectScore;
 	
-	String subjectName;
-	int subjectScore;
-	
-	
-	public String getSubjectName() {
-		return subjectName;
-	}
-	public void setSubjectName(String subjectName) {
-		this.subjectName = subjectName;
-	}
-	public int getSubjectScore() {
-		return subjectScore;
-	}
-	public void setSubjectScore(int subjectScore) {
-		this.subjectScore = subjectScore;
-	}
-	
-	
-	
-	
-	
-	
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public int getSubjectScore() {
+        return subjectScore;
+    }
+
+    public void setSubjectScore(int subjectScore) {
+        this.subjectScore = subjectScore;
+    }
+
 
 }
