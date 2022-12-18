c7aa2ec8e0f12ce53f4c27ac10084df03fc9f9de
chltmdgh522 <chltmdgh517@naver.com>
2022-12-19 01:15:07 +0900

    Chapter5 - classpart(space modify)

--- Chapter5/src/classpart/Student.java
+++ Chapter5/src/classpart/Student.java
@@ -1,31 +1,34 @@
 package classpart;
 
 public class Student {
-	int studentID;
-	String studentName;
-	int grade;
-	String address;
-	
-	public Student() {}
-	public Student(String adress, String name) {
-		address=adress;
-		studentName=name;
-		
-	}
-	
-	public void showStudentInfor() {
-		System.out.println(studentName + ","+ address+ ","+ studentID);
-		
-	}
-	
-	public String getStudentName() {
-		return studentName;
-	 
-}
-	public void setStudentName(String name) {
-		studentName=name;
-		
-	}
+    int studentID;
+    String studentName;
+    int grade;
+    String address;
 
-	
+    public Student() {
+    }
+
+    public Student(String adress, String name) {
+        address = adress;
+        studentName = name;
+
+    }
+
+    public void showStudentInfor() {
+        System.out.println(studentName + "," + address + "," + studentID);
+
+    }
+
+    public String getStudentName() {
+        return studentName;
+
+    }
+
+    public void setStudentName(String name) {
+        studentName = name;
+
+    }
+
+
 }
--- Chapter5/src/classpart/StudentTest.java
+++ Chapter5/src/classpart/StudentTest.java
@@ -2,20 +2,20 @@ package classpart;
 
 public class StudentTest {
 
-	public static void main(String[] args) {
-		
-		Student studentChoi= new Student("d", "최승호");
-		
-		
-		Student studentKim= new Student();
-		studentKim.studentName="김승호";
-		studentKim.studentID=101; 
-		studentKim.address="김포";
-		
-		studentChoi.showStudentInfor();
-		studentKim.showStudentInfor();
-		
-		System.out.println(studentChoi);
-		System.out.println(studentKim );
-	}
+    public static void main(String[] args) {
+
+        Student studentChoi = new Student("d", "최승호");
+
+
+        Student studentKim = new Student();
+        studentKim.studentName = "김승호";
+        studentKim.studentID = 101;
+        studentKim.address = "김포";
+
+        studentChoi.showStudentInfor();
+        studentKim.showStudentInfor();
+
+        System.out.println(studentChoi);
+        System.out.println(studentKim);
+    }
 }
