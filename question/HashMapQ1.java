424c0c953b2edaedfab52227c014dd92f033c0e4
chltmdgh522 <chltmdgh517@naver.com>
2022-12-22 01:21:17 +0900

    Q1

---
+++ question/HashMapQ1.java
@@ -0,0 +1,30 @@
+package question;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class HashMapQ1 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("최승호", 1);
+        hashMap.put("홍세현", 2);
+        hashMap.put("송동화", 3);
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("아이디");
+        String name = s.next();
+        System.out.print("비번");
+        int num = s.nextInt();
+
+        if (hashMap.containsKey(name) == false) {
+            System.out.println("아이디 잘못");
+        } else {
+            if (hashMap.get(name) != num) {
+                System.out.println("비번 잘못");
+            } else {
+                System.out.println("성공");
+            }
+        }
+
+    }
+}
