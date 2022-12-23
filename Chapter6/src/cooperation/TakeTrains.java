b4a0918092b1111e9cc14165ce50a89105cebdee
chltmdgh522 <chltmdgh517@naver.com>
2022-12-24 02:50:57 +0900

    Chapter6(cooperation)-space modify

--- Chapter6/src/cooperation/TakeTrains.java
+++ Chapter6/src/cooperation/TakeTrains.java
@@ -2,23 +2,23 @@ package cooperation;
 
 public class TakeTrains {
 
-	public static void main(String[] args) {
-		Student choi=new Student("최승호",5000);
-		Student kim=new Student("김승호",5000);
-		
-		Bus bus100=new Bus(100);
-		Subway subway1=new Subway(1);
-		
-		choi.takeBus(bus100);
-		choi.takeSubway(subway1);
-		choi.show();
-		
-		kim.takeBus(bus100);
-		kim.takeSubway(subway1);
-		kim.show();
-		
-		bus100.show();
-		subway1.show();
-	}
+    public static void main(String[] args) {
+        Student choi = new Student("최승호", 5000);
+        Student kim = new Student("김승호", 5000);
+
+        Bus bus100 = new Bus(100);
+        Subway subway1 = new Subway(1);
+
+        choi.takeBus(bus100);
+        choi.takeSubway(subway1);
+        choi.show();
+
+        kim.takeBus(bus100);
+        kim.takeSubway(subway1);
+        kim.show();
+
+        bus100.show();
+        subway1.show();
+    }
 
 }
