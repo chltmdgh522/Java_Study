b01bf2daada89440591aa0670d9831b5c4ed6edf
chltmdgh522 <chltmdgh517@naver.com>
2022-12-25 00:50:26 +0900

    Chapter6(singleton) - space modify

--- Chapter6/src/singleton/Company.java
+++ Chapter6/src/singleton/Company.java
@@ -1,15 +1,15 @@
 package singleton;
 
 public class Company {
-	
-	private static Company instance = new Company(); //내부 및 단 한개
-	//private 값이 널이되거나 바꾸는거 방지 static 단 한개 
-	
-	private Company(){
-		System.out.println("d");
-	}//외부에서 이 거를 호출하지 못하게 하겠다.
-	
-	public static Company getInstance() {
-		return instance;// instance 객체를 외부에서 가져다 쓸 수 있다.
-	}
+
+    private static Company instance = new Company(); //내부 및 단 한개
+    //private 값이 널이되거나 바꾸는거 방지 static 단 한개
+
+    private Company() {
+        System.out.println("d");
+    }//외부에서 이 거를 호출하지 못하게 하겠다.
+
+    public static Company getInstance() {
+        return instance;// instance 객체를 외부에서 가져다 쓸 수 있다.
+    }
 }
