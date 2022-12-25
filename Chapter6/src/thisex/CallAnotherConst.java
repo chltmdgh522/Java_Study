c2f1b8af11462455a98e033b94f82daa10b6ae10
chltmdgh522 <chltmdgh517@naver.com>
2022-12-26 00:51:37 +0900

    Chapter6(thisex) - space modify

--- Chapter6/src/thisex/CallAnotherConst.java
+++ Chapter6/src/thisex/CallAnotherConst.java
@@ -1,31 +1,31 @@
 package thisex;
 
-class Person{
-	
-	String name;
-	int age;
-	
-	public Person() {
-		this("이름없음",1);
+class Person {
+
+    String name;
+    int age;
+
+    public Person() {
+        this("이름없음", 1);
 		/*name="이름없음";
 		age=1;
 		*/
-	}
-	
-	public Person(String name, int age) {
-		this.name=name;
-		this.age=age;
-		
-	}
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+    }
 }
 
 public class CallAnotherConst {
 
-	public static void main(String[] args) {
-		
-		Person p1= new Person();
-		System.out.println(p1.name);
+    public static void main(String[] args) {
 
-	}
+        Person p1 = new Person();
+        System.out.println(p1.name);
+
+    }
 
 }
