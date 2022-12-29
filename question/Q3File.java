04574ada32184f2a5a67d1b00bfed984cb028400
chltmdgh522 <chltmdgh517@naver.com>
2022-12-30 01:11:56 +0900

    Q3

---
+++ question/Q3File.java
@@ -0,0 +1,28 @@
+package question;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q3File {
+    public static void main(String[] args) {
+        try(InputStreamReader isr=new InputStreamReader(new FileInputStream("test.txt"))){
+            int i,j = 0,k=0;
+            while((i=isr.read())!=-1){
+                if(i>=65&&i<=90){
+                    j++;
+                }
+                else if(i>=97&&i<=122){
+                    k++;
+                }
+                System.out.print((char)i);
+
+            }
+            System.out.println();
+            System.out.println("소문자:"+ k+" " + "대문자:"+ j);
+        }catch(IOException e){
+            System.out.println(e);
+        }
+
+    }
+}
