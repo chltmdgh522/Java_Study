81a791321476a0f406ffaf39f1d78a04758925d4
chltmdgh522 <chltmdgh517@naver.com>
2022-12-30 01:01:15 +0900

    Chapter7(array) - space modify

--- Chapter7/src/array/ArrayTest2.java
+++ Chapter7/src/array/ArrayTest2.java
@@ -2,21 +2,24 @@ package array;
 
 public class ArrayTest2 {
 
-	public static void main(String[] args) {
-		double[] nume=new double[3]; 
-		
-		int size=0;
-		nume[0]=10.0; size++;
-		nume[1]=20.0; size++;
-		nume[2]=30.0; size++;
-		double total=1;
-		for(int i=0; i<size; i++) {
-			System.out.println(nume[i]);
-			total*=nume[i];
-			
-		}
-		System.out.println(total);
+    public static void main(String[] args) {
+        double[] nume = new double[3];
 
-	}
+        int size = 0;
+        nume[0] = 10.0;
+        size++;
+        nume[1] = 20.0;
+        size++;
+        nume[2] = 30.0;
+        size++;
+        double total = 1;
+        for (int i = 0; i < size; i++) {
+            System.out.println(nume[i]);
+            total *= nume[i];
+
+        }
+        System.out.println(total);
+
+    }
 
 }
--- Chapter7/src/array/ArrayTest3.java
+++ Chapter7/src/array/ArrayTest3.java
@@ -2,21 +2,21 @@ package array;
 
 public class ArrayTest3 {
 
-	public static void main(String[] args) {
-		
-		char[] alphabets=new char[26];
-		
-		char word='A';
-		
-		for(int i=0; i<alphabets.length; i++) {
-			
-			alphabets[i]=word;
-			word++;
-			System.out.println(alphabets[i]);
-		}
-		
+    public static void main(String[] args) {
 
-	}
+        char[] alphabets = new char[26];
+
+        char word = 'A';
+
+        for (int i = 0; i < alphabets.length; i++) {
+
+            alphabets[i] = word;
+            word++;
+            System.out.println(alphabets[i]);
+        }
+
+
+    }
 
 }
  
