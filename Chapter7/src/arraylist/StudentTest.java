a62e981f54f76f40b85be4d7be0cc9bf0867f3fc
chltmdgh522 <chltmdgh517@naver.com>
2023-01-01 01:23:01 +0900

    Chapter7(arraylist) - space modify

--- Chapter7/src/arraylist/StudentTest.java
+++ Chapter7/src/arraylist/StudentTest.java
@@ -2,19 +2,19 @@ package arraylist;
 
 public class StudentTest {
 
-	public static void main(String[] args) {
-		Student seunghoChoi=new Student(1,"최승호");
-		seunghoChoi.addSubject("국어",100);
-		seunghoChoi.addSubject("수학",100);
-		
-		
-		Student minsuLee=new Student(2,"이민수");
-		minsuLee.addSubject("국어",19);
-		minsuLee.addSubject("수학",17);
-		
-		seunghoChoi.show();
-		minsuLee.show();
+    public static void main(String[] args) {
+        Student seunghoChoi = new Student(1, "최승호");
+        seunghoChoi.addSubject("국어", 100);
+        seunghoChoi.addSubject("수학", 100);
 
-		
-	}
+
+        Student minsuLee = new Student(2, "이민수");
+        minsuLee.addSubject("국어", 19);
+        minsuLee.addSubject("수학", 17);
+
+        seunghoChoi.show();
+        minsuLee.show();
+
+
+    }
 }
