b36edc8e526ae99af286aa7783a473be7ebde585
chltmdgh522 <chltmdgh517@naver.com>
2023-01-02 00:24:03 +0900

    Q4

---
+++ question/Q4Channo.java
@@ -0,0 +1,22 @@
+package question;
+
+import java.util.Scanner;
+
+public class Q4Channo {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("입력:");
+        String str= sc.next();
+        int cnt=0;
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i)=='a'){
+                cnt++;
+            }
+        }
+
+        System.out.printf("a의 갯수"+cnt);
+
+
+
+    }
+}
