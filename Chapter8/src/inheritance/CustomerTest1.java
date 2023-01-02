eda544b7cf41ed679606f105d8c89416a4beafd7
chltmdgh522 <chltmdgh517@naver.com>
2023-01-03 00:27:33 +0900

    Chapter8(inheritance)

--- Chapter8/src/inheritance/CustomerTest1.java
+++ Chapter8/src/inheritance/CustomerTest1.java
@@ -2,11 +2,11 @@ package inheritance;
 
 public class CustomerTest1 {
 
-	public static void main(String[] args) {
-		Customer seunghoChoi=new Customer(11,"최승호");
-		int price=seunghoChoi.calcPrice(10000);
-		
-		VIPCustomer minsuLee=new VIPCustomer(12,"이민수",11);
+    public static void main(String[] args) {
+        Customer seunghoChoi = new Customer(11, "최승호");
+        int price = seunghoChoi.calcPrice(10000);
+
+        VIPCustomer minsuLee = new VIPCustomer(12, "이민수", 11);
 		
 		
 		/*Customer minsuLee=new VIPCustomer(12,"이민수");
@@ -14,15 +14,12 @@ public class CustomerTest1 {
 		 이해 안간다. 타빕이 Customer이기 때무에 변수나 메서드도 Customer임
 		 */
 
-		minsuLee.bonusPoint=1000;
-		
-		
-		
-		
-		
-		System.out.println(seunghoChoi.show()+price);
-		System.out.println(minsuLee.show());
+        minsuLee.bonusPoint = 1000;
 
-	}
+
+        System.out.println(seunghoChoi.show() + price);
+        System.out.println(minsuLee.show());
+
+    }
 
 }
