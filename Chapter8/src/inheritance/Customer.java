8216f0cc98ea2fd6eb9fc2181e1b007840254d4f
chltmdgh522 <chltmdgh517@naver.com>
2023-01-03 00:27:21 +0900

    Chapter8(inheritance)

--- Chapter8/src/inheritance/Customer.java
+++ Chapter8/src/inheritance/Customer.java
@@ -1,12 +1,12 @@
 package inheritance;
 
 public class Customer {
-	
-	protected int customerID; // protected는 상속관계에서 쓸수 있음 추가로 패키지가 달라도 쓸수 있음 
-	protected String customerName;
-	protected String customerGrade;
-	int bonusPoint;
-	double bonusRatio;
+
+    protected int customerID; // protected는 상속관계에서 쓸수 있음 추가로 패키지가 달라도 쓸수 있음
+    protected String customerName;
+    protected String customerGrade;
+    int bonusPoint;
+    double bonusRatio;
 	
 
 	
@@ -16,53 +16,49 @@ public class Customer {
 		customerGrade="SILVER";
 		bonusRatio=0.01;
 	}*/
-	
-	public Customer(int customerID, String customerName) {
-		this.customerID=customerID;
-		this.customerName=customerName;
-		customerGrade="SILVER";
-		bonusRatio=0.01;	
-	}
-	
-	public int calcPrice(int price) {
-		if(customerGrade=="SILVER") {
-		bonusPoint +=price*bonusRatio;
-		}
-		
-		return price;
-	}
-	
-	public String show() {
-		return customerName+"님의 등급은"+customerGrade+" 입니다."+"  "+bonusPoint+"적립되었습니다.";
-	}
 
-	public int getCustomerID() {
-		return customerID;
-	}
+    public Customer(int customerID, String customerName) {
+        this.customerID = customerID;
+        this.customerName = customerName;
+        customerGrade = "SILVER";
+        bonusRatio = 0.01;
+    }
 
-	public void setCustomerID(int customerID) {
-		this.customerID = customerID;
-	}
+    public int calcPrice(int price) {
+        if (customerGrade == "SILVER") {
+            bonusPoint += price * bonusRatio;
+        }
 
-	public String getCustomerName() {
-		return customerName;
-	}
+        return price;
+    }
 
-	public void setCustomerName(String customerName) {
-		this.customerName = customerName;
-	}
+    public String show() {
+        return customerName + "님의 등급은" + customerGrade + " 입니다." + "  " + bonusPoint + "적립되었습니다.";
+    }
 
-	public String getCustomerGrade() {
-		return customerGrade;
-	}
+    public int getCustomerID() {
+        return customerID;
+    }
 
-	public void setCustomerGrade(String customerGrade) {
-		this.customerGrade = customerGrade;
-	}
-	
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
 
-	
-	
-	
-	
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerGrade() {
+        return customerGrade;
+    }
+
+    public void setCustomerGrade(String customerGrade) {
+        this.customerGrade = customerGrade;
+    }
+
+
 }
