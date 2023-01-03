63e0c7a7c1bccc4d9b28055c8a5f8ce7198de045
chltmdgh522 <chltmdgh517@naver.com>
2023-01-04 00:00:42 +0900

    Chapter8(inheritance)

--- Chapter8/src/inheritance/OverridingTest.java
+++ Chapter8/src/inheritance/OverridingTest.java
@@ -2,25 +2,25 @@ package inheritance;
 
 public class OverridingTest {
 
-	public static void main(String[] args) {
-		
-		Customer Moon=new Customer(1,"문홍욱");
-		int price=Moon.calcPrice(10000);
-		System.out.println(Moon.show()+price);
-		
-		
-		VIPCustomer Kim=new VIPCustomer(2,"김용현",11);
-		price=Kim.calcPrice(10000);
-		System.out.println(Kim.show()+price+Kim.getAgentID());
-		
-		Customer seunghoChoi=new Customer(3,"최승호");
-		price=seunghoChoi.calcPrice(10000);
-		System.out.println(seunghoChoi.show()+price);
-		
-		VIPCustomer minsuLee=new VIPCustomer(4,"이민수",11);
-		price=minsuLee.calcPrice(10000);
-		System.out.println(minsuLee.show()+price);
+    public static void main(String[] args) {
 
-	}
+        Customer Moon = new Customer(1, "문홍욱");
+        int price = Moon.calcPrice(10000);
+        System.out.println(Moon.show() + price);
+
+
+        VIPCustomer Kim = new VIPCustomer(2, "김용현", 11);
+        price = Kim.calcPrice(10000);
+        System.out.println(Kim.show() + price + Kim.getAgentID());
+
+        Customer seunghoChoi = new Customer(3, "최승호");
+        price = seunghoChoi.calcPrice(10000);
+        System.out.println(seunghoChoi.show() + price);
+
+        VIPCustomer minsuLee = new VIPCustomer(4, "이민수", 11);
+        price = minsuLee.calcPrice(10000);
+        System.out.println(minsuLee.show() + price);
+
+    }
 
 }
