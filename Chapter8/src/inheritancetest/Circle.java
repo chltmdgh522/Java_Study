17dea89e60a9c6a1b9b8313788de20a31a1eadcb
chltmdgh522 <chltmdgh517@naver.com>
2023-01-07 01:40:01 +0900

    Chapter8(inheritancetest)

--- Chapter8/src/inheritancetest/Circle.java
+++ Chapter8/src/inheritancetest/Circle.java
@@ -1,14 +1,14 @@
 package inheritancetest;
+
 public class Circle {
-	
-	Point point; // has의 관계 
-	
-	private int radius;
-	
-	public Circle() {
-		point=new Point();
-	}
+
+    Point point; // has의 관계
+
+    private int radius;
+
+    public Circle() {
+        point = new Point();
+    }
 
 
-	
 }
