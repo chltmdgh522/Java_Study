a2d2569d99223e8f5813731c920e0fbe8f689b32
chltmdgh522 <chltmdgh517@naver.com>
2023-01-08 01:46:11 +0900

    study4 - hashset

--- Study/src/study4collection/hashset/HashSetTest.java
+++ Study/src/study4collection/hashset/HashSetTest.java
@@ -4,17 +4,18 @@ import study4collection.Member;
 
 public class HashSetTest {
     public static void main(String[] args) {
-        MemberHashSet m=new MemberHashSet();
-        Member m1 =new Member(2,"최승호");
-        Member m2 =new Member(3,"홍세현");
-        Member m3 =new Member(1,"명승진");
-        m.addHashSet(m1);
-        m.addHashSet(m2);
-        m.addHashSet(m3);
-        m.removeHashSet(1);
 
-        m.show();
+        MemberHashSet memberHashSet=new MemberHashSet();
+        Member m1=new Member(1,"최승호");
+        Member m2=new Member(2,"홍세현");
+        Member m3=new Member(3,"칠라디");
+        memberHashSet.addHashSet(m1);
+        memberHashSet.addHashSet(m2);
+        memberHashSet.addHashSet(m3);
+        memberHashSet.removeHashSet(m1.getId());
+        //memberHashSet.removeHashSet(m1);
 
+        memberHashSet.show();
 
 
     }
