258fa5bc8349005c68b426b20b82583f1cbe5c77
chltmdgh522 <chltmdgh517@naver.com>
2023-01-13 00:53:40 +0900

    study6 - space modify

--- Study/src/study6newstream/writer/FileWriterTest.java
+++ Study/src/study6newstream/writer/FileWriterTest.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class FileWriterTest {
     public static void main(String[] args) {
         char[] ch={'a','b'};
-        try(FileWriter fw=new FileWriter("writer.txt")){
+        try(FileWriter fw=new FileWriter("writer.txt",true)){
             fw.write("승호는 귀여어요");
             fw.write(ch);
         }catch(IOException e){
