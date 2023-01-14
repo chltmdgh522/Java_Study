c9884d04363293cc1069f18315e27fd3f5caf083
chltmdgh522 <chltmdgh517@naver.com>
2023-01-15 00:11:06 +0900

    Q2 - space modify

--- question/Q2File.java
+++ question/Q2File.java
@@ -8,33 +8,32 @@ import java.util.Scanner;
 
 public class Q2File {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        try(FileWriter fw=new FileWriter("file.txt")){
-            String str=sc.next();
+        Scanner sc = new Scanner(System.in);
+        try (FileWriter fw = new FileWriter("file.txt")) {
+            String str = sc.next();
             fw.write(str);
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println(e);
         }
 
-        try(InputStreamReader irs=new InputStreamReader(new FileInputStream("file.txt"))){
+        try (InputStreamReader irs = new InputStreamReader(new FileInputStream("file.txt"))) {
             int i;
-            String b="";
-            String c="";
-            while((i=irs.read())!=-1){
-                System.out.print((char)i);
-                b+= String.valueOf((char)i);
+            StringBuilder b = new StringBuilder();
+            StringBuilder c = new StringBuilder();
+            while ((i = irs.read()) != -1) {
+                System.out.print((char) i);
+                b.append(String.valueOf((char) i));
             }
-            for(int j=b.length(); j>0; j--){
-                c+=b.charAt(j-1);
+            for (int j = b.length(); j > 0; j--) {
+                c.append(b.charAt(j - 1));
             }
-            if(b.equals(c)){
+            if (b.toString().equals(c.toString())) {
                 System.out.println("화문수맞음");
-            }
-            else{
+            } else {
                 System.out.println("틀림");
             }
 
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println(e);
         }
 
