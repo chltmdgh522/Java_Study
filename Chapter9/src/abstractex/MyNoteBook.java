6fd10bb0beff19b5d43547bbfbb0042355854111
chltmdgh522 <chltmdgh517@naver.com>
2023-01-20 00:44:36 +0900

    Chapter9 - space modify

--- Chapter9/src/abstractex/MyNoteBook.java
+++ Chapter9/src/abstractex/MyNoteBook.java
@@ -2,12 +2,11 @@ package abstractex;
 
 public class MyNoteBook extends NoteBook {
 
-	@Override
-	public void typing() {
-		System.out.println("NoteBook typing()");
-		
-	}
-	
-	
+    @Override
+    public void typing() {
+        System.out.println("NoteBook typing()");
+
+    }
+
 
 }
