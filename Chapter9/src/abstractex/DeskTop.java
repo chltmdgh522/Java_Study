4544d96c6fb11c319f6724bf5341723c9b8f0d31
chltmdgh522 <chltmdgh517@naver.com>
2023-01-20 00:44:20 +0900

    Chapter9 - space modify

--- Chapter9/src/abstractex/DeskTop.java
+++ Chapter9/src/abstractex/DeskTop.java
@@ -1,15 +1,16 @@
 package abstractex;
 
-public class DeskTop extends Computer{
+public class DeskTop extends Computer {
 
-	@Override
-	public void display() {
-		System.out.println("DeskTop display");
-	}
-	@Override
-	public void typing() {
-		System.out.println("DeskTop typing");
-		
-	}
+    @Override
+    public void display() {
+        System.out.println("DeskTop display");
+    }
+
+    @Override
+    public void typing() {
+        System.out.println("DeskTop typing");
+
+    }
 
 }
