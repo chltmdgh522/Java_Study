66985c93b5192cc169b7662a228f47d9d60c0b37
chltmdgh522 <chltmdgh517@naver.com>
2023-01-23 02:10:45 +0900

    Chapter9 - space modify

--- Chapter9/src/gamelevel/MainBoard.java
+++ Chapter9/src/gamelevel/MainBoard.java
@@ -2,19 +2,19 @@ package gamelevel;
 
 public class MainBoard {
 
-	public static void main(String[] args) {
-		
-		Player player1= new Player();
-		player1.playe(1);
-		SuperLevel sLevel= new SuperLevel();
-		player1.upgradeLevel(sLevel);
-		player1.playe(3);
+    public static void main(String[] args) {
 
-		AdvancedLevel aLevel= new AdvancedLevel();
-		player1.upgradeLevel(aLevel);
-		player1.playe(2);
-		
-		
-	}
+        Player player1 = new Player();
+        player1.playe(1);
+        SuperLevel sLevel = new SuperLevel();
+        player1.upgradeLevel(sLevel);
+        player1.playe(3);
+
+        AdvancedLevel aLevel = new AdvancedLevel();
+        player1.upgradeLevel(aLevel);
+        player1.playe(2);
+
+
+    }
 
 }
