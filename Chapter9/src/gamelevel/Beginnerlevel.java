e772679a9c9341779d91440bd02d80a49e6a923e
chltmdgh522 <chltmdgh517@naver.com>
2023-01-23 02:10:35 +0900

    Chapter9 - space modify

--- Chapter9/src/gamelevel/Beginnerlevel.java
+++ Chapter9/src/gamelevel/Beginnerlevel.java
@@ -2,28 +2,28 @@ package gamelevel;
 
 public class Beginnerlevel extends PlayerLevel {
 
-	@Override
-	public void run() {
-		System.out.println("천천히 달립니다");
+    @Override
+    public void run() {
+        System.out.println("천천히 달립니다");
 
-	}
+    }
 
-	@Override
-	public void jump() {
-		System.out.println("점프 못합니다");
+    @Override
+    public void jump() {
+        System.out.println("점프 못합니다");
 
-	}
+    }
 
-	@Override
-	public void turn() {
-		System.out.println("턴도 못합니다.");
+    @Override
+    public void turn() {
+        System.out.println("턴도 못합니다.");
 
-	}
+    }
 
-	@Override
-	public void showLevelMessage() {
-		System.out.println("초보자입니다.");
+    @Override
+    public void showLevelMessage() {
+        System.out.println("초보자입니다.");
 
-	}
+    }
 
 }
