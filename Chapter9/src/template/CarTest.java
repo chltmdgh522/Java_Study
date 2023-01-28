f9f0b32a1ca7b8f46536f850c3b5cff03a2d6bfc
chltmdgh522 <chltmdgh517@naver.com>
2023-01-28 20:13:21 +0900

    Chapter9 - space modify

--- Chapter9/src/template/CarTest.java
+++ Chapter9/src/template/CarTest.java
@@ -2,14 +2,14 @@ package template;
 
 public class CarTest {
 
-	public static void main(String[] args) {
-		Car car1= new ManualCar();
-		AICar car2= new AICar();
-		
-		car1.run();
-		System.out.println("====================");
-		car2.run();
+    public static void main(String[] args) {
+        Car car1 = new ManualCar();
+        AICar car2 = new AICar();
 
-	}
+        car1.run();
+        System.out.println("====================");
+        car2.run();
+
+    }
 
 }
