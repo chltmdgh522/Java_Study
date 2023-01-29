331173b920f8c99bb995030559e98c42930d1cf3
chltmdgh522 <chltmdgh517@naver.com>
2023-01-30 00:08:59 +0900

    Chapter10 - space modify

--- Chapter10/src/bookshelf/Queue.java
+++ Chapter10/src/bookshelf/Queue.java
@@ -1,9 +1,11 @@
 package bookshelf;
 
 public interface Queue {
-	
-	void enQueue(String title);
-	String deQueue();
-	int getSize();
+
+    void enQueue(String title);
+
+    String deQueue();
+
+    int getSize();
 
 }
