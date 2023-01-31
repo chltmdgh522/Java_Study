492dab61df345ec20a30c7aa08ca4805314a3fdb
chltmdgh522 <chltmdgh517@naver.com>
2023-01-31 11:42:59 +0900

    Chapter10 - space modify

--- Chapter10/src/bookshelf/Shelf.java
+++ Chapter10/src/bookshelf/Shelf.java
@@ -3,18 +3,18 @@ package bookshelf;
 import java.util.ArrayList;
 
 public class Shelf {
-	
-	protected ArrayList<String> shelf;
-	
-	public Shelf() {
-		Object
-		shelf=new ArrayList<String>();
-	}
-	
-	public ArrayList<String> getShelf(){
-		return shelf;
-		
-	}
-	
+
+    protected ArrayList<String> shelf;
+
+    public Shelf() {
+        Object
+                shelf = new ArrayList<String>();
+    }
+
+    public ArrayList<String> getShelf() {
+        return shelf;
+
+    }
+
 
 }
