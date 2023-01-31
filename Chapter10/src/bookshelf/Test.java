eb04d289f34f01ba4b92e232491845ee6be40291
chltmdgh522 <chltmdgh517@naver.com>
2023-01-31 11:43:17 +0900

    Chapter10 - space modify

--- Chapter10/src/bookshelf/Test.java
+++ Chapter10/src/bookshelf/Test.java
@@ -2,16 +2,16 @@ package bookshelf;
 
 public class Test {
 
-	public static void main(String[] args) {
-		Queue sq=new BookShelf();
-		
-		sq.enQueue("고구려");
-		sq.enQueue("발해");
-		sq.enQueue("고려");
-		System.out.println(sq.deQueue());
-		System.out.println(sq.deQueue());
-		System.out.println(sq.getSize());
+    public static void main(String[] args) {
+        Queue sq = new BookShelf();
 
-	}
+        sq.enQueue("고구려");
+        sq.enQueue("발해");
+        sq.enQueue("고려");
+        System.out.println(sq.deQueue());
+        System.out.println(sq.deQueue());
+        System.out.println(sq.getSize());
+
+    }
 
 }
