7c851845b1b04685fb93585bc5ae65a6188f94bb
chltmdgh522 <chltmdgh517@naver.com>
2023-01-31 23:57:28 +0900

    Chapter10 - space modify

--- Chapter10/src/inherbit/MyClass.java
+++ Chapter10/src/inherbit/MyClass.java
@@ -2,31 +2,31 @@ package inherbit;
 
 public class MyClass implements MyInterface {
 
-	@Override
-	public void x() {
-		System.out.println("X");
-		
-	}
+    @Override
+    public void x() {
+        System.out.println("X");
 
-	@Override
-	public void y() {
-		System.out.println("Y");
-		
-	}
+    }
 
-	@Override
-	public void myMethod() {
-		System.out.println("mymethod");
-		
-	}
-	
-	public static void main(String[] args) {
-		MyClass my=new MyClass();
-		
-		X x=my;
-		x.x();
-		my.myMethod();
-		
-	}
+    @Override
+    public void y() {
+        System.out.println("Y");
+
+    }
+
+    @Override
+    public void myMethod() {
+        System.out.println("mymethod");
+
+    }
+
+    public static void main(String[] args) {
+        MyClass my = new MyClass();
+
+        X x = my;
+        x.x();
+        my.myMethod();
+
+    }
 
 }
