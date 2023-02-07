eb42df0c81580e2c9568ba552bd8331eca22477b
chltmdgh522 <chltmdgh517@naver.com>
2023-02-08 00:00:39 +0900

    study4 - space modify

--- Study/src/study4collection/hashset/HashSetTest.java
+++ Study/src/study4collection/hashset/HashSetTest.java
@@ -5,18 +5,15 @@ import study4collection.Member;
 public class HashSetTest {
     public static void main(String[] args) {
 
-        MemberHashSet memberHashSet=new MemberHashSet();
-        Member m1=new Member(1,"최승호");
-        Member m2=new Member(4 ,"홍세현");
-        Member m3=new Member(3,"칠라디");
+        MemberHashSet memberHashSet = new MemberHashSet();
+        Member m1 = new Member(1, "최승호");
+        Member m2 = new Member(4, "홍세현");
+        Member m3 = new Member(3, "칠라디");
         memberHashSet.addHashSet(m1);
         memberHashSet.addHashSet(m2);
         memberHashSet.addHashSet(m3);
         memberHashSet.removeHashSet(m1.getId());
         //memberHashSet.removeHashSet(m1);
-
         memberHashSet.show();
-
-
     }
 }
