4725f645199c44c16657f46a142013eafe3f2adf
chltmdgh522 <chltmdgh517@naver.com>
2023-02-09 01:14:19 +0900

    test3

---
+++ test0/test3.java
@@ -0,0 +1,27 @@
+package test0;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class test3 {
+    public static void main(String[] args) {
+        String[] num = {"1", "2", "3", "1"};
+        Map<String, Integer> hashMap = new HashMap<>();
+
+        for (String key : num) {
+            hashMap.put(key, hashMap.getOrDefault(key, 0) + 1);
+        }
+
+        for(Map.Entry<String,Integer> entrySet: hashMap.entrySet()){
+            System.out.println(entrySet.getKey());
+        }
+
+        /*
+        결과값:
+        1:2
+        2:1
+        3:1
+        */
+    }
+}
