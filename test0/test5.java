51512b62b99ff12910920f87d898ee9457c67dce
chltmdgh522 <chltmdgh517@naver.com>
2023-03-15 11:16:26 +0900

    test5

--- test0/test5.java
+++ test0/test5.java
@@ -1,7 +1,20 @@
 package test0;
 
+import jdk.jshell.JShell;
+
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+
 public class test5 {
     public static void main(String[] args) {
+        HashMap<Integer, String> hashMap=new HashMap<>();
+        hashMap.put(2,"choi");
+        hashMap.put(1,"chi");
+        hashMap.put(1,"chi");
+        for(Integer key: hashMap.keySet()){
+            System.out.println(hashMap.get(key));
+        }
 
-    }
+     }
 }
