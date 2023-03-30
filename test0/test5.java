bd8eb987d522d635c143b6c4179584e3bb5596f9
chltmdgh522 <chltmdgh517@naver.com>
2023-03-30 13:42:49 +0900

    test5,6

--- test0/test5.java
+++ test0/test5.java
@@ -5,16 +5,20 @@ import jdk.jshell.JShell;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.Stack;
 
 public class test5 {
     public static void main(String[] args) {
-        HashMap<Integer, String> hashMap=new HashMap<>();
-        hashMap.put(2,"choi");
-        hashMap.put(1,"chi");
-        hashMap.put(1,"chi");
-        for(Integer key: hashMap.keySet()){
-            System.out.println(hashMap.get(key));
+        int[] data=new int[5];
+        data= new int[]{1, 2, 3, 4, 5};
+        data= new int[]{1,2,3};
+//c언어는 저거 오류남
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
         }
 
+        Stack<Integer> stack= new Stack<>();
+        stack.push(-1);
+        System.out.println(stack.get(0));
      }
 }
